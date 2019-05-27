33f750013431972550e386028f986521f9c545a3
zhangguo <506430706@qq.com>
2019-05-27 10:44:37 +0800

    首次提交 router store gzip压缩配置等等

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <A msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import A from '@/components/A.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    A
   }
 }
 </script>
